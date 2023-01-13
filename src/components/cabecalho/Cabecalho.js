fafdd03862aa50a7371068161274e2d927fd78d8
Jessicamf4 <jessicamarquesfialho0@gmail.com>
2023-01-13 18:12:20 -0300

    Add img e links

--- src/components/cabecalho/Cabecalho.js
+++ src/components/cabecalho/Cabecalho.js
@@ -7,13 +7,14 @@ export default function Cabecalho(){
     return(
         <nav className="navbar">
             <span className="nome">Jéssica Marques, 18 anos</span>
-            <a className="buttonGit" href="http://google.com" target="_blank">
+            <a className="buttonGit" href="https://github.com/Jessicamf4" target="_blank">
                 <AiFillGithub size={25} color="FFF"/>
             </a>   
 
-            <a className="buttonLinke" href="http://google.com" target="_blank">
+            <a className="buttonLinke" href="https://www.linkedin.com/in/jéssica-marques-fialho/" target="_blank">
                 <AiFillLinkedin size={25} color="FFF"/>
             </a>    
         </nav>
     );
 }
+
--- src/components/projetos/Projetos.js
+++ src/components/projetos/Projetos.js
@@ -1,6 +1,11 @@
 import React from "react";
 import "./Projetos.css";
-import JV from "./../img/imgJV.png"
+import JV from "./../img/jogoVelha.jpg"
+import JM from "./../img/memoria.jpg"
+import Cal from "./../img/calculadora2.jpg"
+import Dados from "./../img/dados.jpg"
+import Lista from "./../img/listaTarefas.jpg"
+import Proj from "./../img/projRe.jpg"
 
 export default function Projetos(){
     return(
@@ -9,45 +14,49 @@ export default function Projetos(){
 
             <div className="g1">
                 <div className="container">
-                    <p className="tituloProjeto">Bot do Telegram </p> 
+                    <p className="tituloProjeto">Jogo da Velha </p> 
                     <img className="img2" src={JV} />
-                    <button>Site</button>
+                    <button href="https://jessicamf4.github.io/Calculadora/" target="_blank">Site</button>
                 </div>
                     <p className="quebra">.</p>
                 <div className="container">
                     <p className="tituloProjeto">Jogo da Memória</p>
-                    <img className="img2" src={JV} />
-                    <button>Site</button>
+                    <img className="img2" src={JM} />
+                    <button href="https://jessicamf4.github.io/Calculadora/" target="_blank">Site</button>
                 </div>
                 <p className="quebra">.</p>
 
                 <div className="container">
                     <p className="tituloProjeto">Análise de Dados</p>
-                    <img className="img2" src={JV} />
-                    <button>Site</button>
+                    <img className="img2" src={Dados} />
+                    <button href="https://github.com/Jessicamf4/Analise-de-Dados" target="_blank">Site</button>
                 </div>
                 <p className="quebra">.</p>
 
                 <div className="container">
-                    <p className="tituloProjeto">Bot Telegram </p> 
-                    <img className="img2" src={JV} />
-                    <button>Site</button>
+                    <p className="tituloProjeto">Calculadora </p> 
+                    <img className="img2" src={Cal} />
+                    <button href="https://jessicamf4.github.io/Calculadora/" target="_blank">Site</button>
                 </div>
                 <p className="quebra">.</p>
 
                 <div className="container">
-                    <p className="tituloProjeto">Jogo da Memória</p>
-                    <img className="img2" src={JV} />
-                    <button>Site</button>
+                    <p className="tituloProjeto">Lista de Tarefas</p>
+                    <img className="img2" src={Lista} />
+                    <button href="https://jessicamf4.github.io/Calculadora/" target="_blank">Site</button>
                 </div>
                 <p className="quebra">.</p>
                 
                 <div className="container">
-                    <p className="tituloProjeto">Análise de Dados</p>
-                    <img className="img2" src={JV} />
-                    <button>Site</button>
+                    <p className="tituloProjeto">Projeto Reciclagem</p>
+                    <img className="img2" src={Proj} />
+                    <button href="https://jessicamf4.github.io/Projeto-Reciclagem/" target="_blank">Site</button>
                 </div>
             </div>
         </nav>
     );
 }
+
+//https://github.com/Jessicamf4/Buscador-Cep
+
+//https://github.com/Jessicamf4/Bot-Telegram
